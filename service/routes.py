3b961c0ee029c7f9498177c0c48cc28af17a6818
Thera <thera.lombardi@gmail.com>
2025-12-18 15:51:16 -0500

    EDit for flaks lint

--- service/routes.py
+++ service/routes.py
@@ -61,6 +61,7 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -85,9 +86,7 @@ def read_account(account_id):
     This endpoint will read an Account based the account_id requested
     """
     app.logger.info("Request to read an Account with id: %s", account_id)
-    
     account = Account.find(account_id)
-    
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
@@ -95,6 +94,7 @@ def read_account(account_id):
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
+
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
@@ -114,6 +114,7 @@ def update_accounts(account_id):
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
+
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
